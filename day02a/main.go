5eef7ae9097655f95df2a1c94e2e0f27cc2d2034
Johan Bogema <31311380+mrboogiee@users.noreply.github.com>
2024-12-10 13:37:34 +0100

    pulled the logic apart into multiple sub funcs

--- day02a/main.go
+++ day02a/main.go
@@ -11,40 +11,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	regexGetDigits := regexp.MustCompile(`(\d*)`)
 	// left, right := []int{}, []int{}
 	invalids := 0
 	for _, line := range lines {
-		digits := regexGetDigits.FindAllString(line, -1)
-		direction := "none"
-		lastDigit := 0
-		valid := true
-		for i, digit := range digits {
-			intValue, _ := strconv.Atoi(digit)
-			if i == 0 {
-				lastDigit = intValue
-			} else if i == 1 {
-				direction = getDirection(lastDigit, intValue)
-			} else if i > 1 {
-				newDirection := getDirection(lastDigit, intValue)
-				if newDirection != direction {
-					valid = false
-				}
-			}
-			if valid && i > 0 {
-				distance := getDistance(lastDigit, intValue)
-				if distance < 1 || distance > 3 {
-					valid = false
-				}
-			}
-			lastDigit = intValue
-		}
-		if !valid {
-			invalids++
-		}
+		invalids = processLine(line, invalids)
 	}
-	fmt.Println(len(lines))
-	fmt.Println(invalids)
 	fmt.Println(len(lines) - invalids)
 }
 
@@ -64,4 +35,45 @@ func getDistance(first, second int) int {
 	} else {
 		return second - first
 	}
+}
+
+func processLine(line string, invalids int) int {
+	regexGetDigits := regexp.MustCompile(`(\d*)`)
+	digits := regexGetDigits.FindAllString(line, -1)
+	valid := processDigits(digits)
+	if !valid {
+		invalids++
+	}
+	return invalids
+}
+
+func processDigits(digits []string) bool {
+	valid := true
+	lastDigit := 0
+	direction := "none"
+	for i, digit := range digits {
+		valid, lastDigit, direction = processDigit(i, digit, lastDigit, direction, valid)
+	}
+	return valid
+}
+func processDigit(index int, digit string, lastDigit int, direction string, valid bool) (bool, int, string) {
+	intValue, _ := strconv.Atoi(digit)
+	if index == 0 {
+		lastDigit = intValue
+	} else if index == 1 {
+		direction = getDirection(lastDigit, intValue)
+	} else if index > 1 {
+		newDirection := getDirection(lastDigit, intValue)
+		if newDirection != direction {
+			valid = false
+		}
+	}
+	if valid && index > 0 {
+		distance := getDistance(lastDigit, intValue)
+		if distance < 1 || distance > 3 {
+			valid = false
+		}
+	}
+	lastDigit = intValue
+	return valid, lastDigit, direction
 }
